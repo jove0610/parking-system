7964fb07e19bd1a2b5ea5df67c5df919804fb0db
Jose Anthony Jovellanos <jove0610@gmail.com>
2022-07-01 04:56:18 +0800

    create dialog when parking

---
+++ src/components/DialogPark.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+function BootstrapDialogTitle(props) {
+  const { children, onClose, ...other } = props;
+
+  return (
+    // eslint-disable-next-line
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
+
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+function DialogPark({ setOpen, data }) {
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open
+      >
+        <Box width="30em" maxWidth="90vw">
+          <BootstrapDialogTitle
+            id="customized-dialog-title"
+            onClose={handleClose}
+          >
+            Welcome...
+          </BootstrapDialogTitle>
+
+          <DialogContent dividers>
+            <Stack spacing="2em">
+              <Typography gutterBottom>
+                <span style={{ fontWeight: 'bold' }}>Parking No: &nbsp;</span>
+                {data.id}
+              </Typography>
+              <Typography gutterBottom>
+                <span style={{ fontWeight: 'bold' }}>Start: &nbsp;</span>
+                {dayjs(data.startTime).format('MMM D, h:mm:ss A')}
+              </Typography>
+            </Stack>
+          </DialogContent>
+
+          <DialogActions>
+            <Button variant="contained" autoFocus onClick={handleClose}>
+              Ok
+            </Button>
+          </DialogActions>
+        </Box>
+      </BootstrapDialog>
+    </div>
+  );
+}
+
+DialogPark.propTypes = {
+  setOpen: PropTypes.func.isRequired,
+  data: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    startTime: PropTypes.string.isRequired,
+    endTime: PropTypes.string.isRequired,
+    amount: PropTypes.number.isRequired,
+    totalAmountPaid: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default DialogPark;
--- src/components/ParkVehicleForm.jsx
+++ src/components/ParkVehicleForm.jsx
@@ -5,10 +5,13 @@ import { Autocomplete, Button, Stack, TextField } from '@mui/material';
 import usePrepareParkData from '../helpers/usePrepareParkData';
 import { park } from '../redux/parkingSpotsSlice';
 import { parkEntranceOptions, sizeOptions } from '../constants';
+import DialogPark from './DialogPark';
 
 function ParkVehicleForm() {
   const dispatch = useDispatch();
   const prepareParkData = usePrepareParkData();
+  const [openDialog, setOpenDialog] = useState(false);
+  const [dialogData, setDialogData] = useState({});
   const [plateNo, setPlateNo] = useState('');
   const [size, setSize] = useState(null);
   const [parkEntrance, setParkEntrance] = useState(null);
@@ -18,55 +21,66 @@ function ParkVehicleForm() {
     const sizeId = size.id;
     const parkEntranceId = parkEntrance.id;
     const data = prepareParkData({ plateNo, sizeId, parkEntranceId });
+
     dispatch(park(data));
+    setDialogData(data);
+    setOpenDialog(true);
+
+    setPlateNo('');
+    setSize(null);
+    setParkEntrance(null);
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <Stack spacing="2em" alignItems="center" mt="2em">
-        <TextField
-          value={plateNo}
-          onChange={(e) => setPlateNo(e.target.value)}
-          label="Plate No."
-          size="small"
-          required
-        />
+    <>
+      <form onSubmit={onSubmit}>
+        <Stack spacing="2em" alignItems="center" mt="2em">
+          <TextField
+            value={plateNo}
+            onChange={(e) => setPlateNo(e.target.value)}
+            label="Plate No."
+            size="small"
+            required
+          />
 
-        <Autocomplete
-          value={size}
-          options={sizeOptions}
-          onChange={(_, newValue) => setSize(newValue)}
-          sx={{ width: 250 }}
-          renderInput={(params) => (
-            <TextField
-              {...params} // eslint-disable-line
-              label="Vehicle Size"
-              size="small"
-              required
-            />
-          )}
-        />
+          <Autocomplete
+            value={size}
+            options={sizeOptions}
+            onChange={(_, newValue) => setSize(newValue)}
+            sx={{ width: 250 }}
+            renderInput={(params) => (
+              <TextField
+                {...params} // eslint-disable-line
+                label="Vehicle Size"
+                size="small"
+                required
+              />
+            )}
+          />
 
-        <Autocomplete
-          value={parkEntrance}
-          options={parkEntranceOptions}
-          onChange={(_, newValue) => setParkEntrance(newValue)}
-          sx={{ width: 250 }}
-          renderInput={(params) => (
-            <TextField
-              {...params} // eslint-disable-line
-              label="Parking Entrance"
-              size="small"
-              required
-            />
-          )}
-        />
+          <Autocomplete
+            value={parkEntrance}
+            options={parkEntranceOptions}
+            onChange={(_, newValue) => setParkEntrance(newValue)}
+            sx={{ width: 250 }}
+            renderInput={(params) => (
+              <TextField
+                {...params} // eslint-disable-line
+                label="Parking Entrance"
+                size="small"
+                required
+              />
+            )}
+          />
 
-        <Button variant="contained" type="submit">
-          Park Vehicle
-        </Button>
-      </Stack>
-    </form>
+          <Button variant="contained" type="submit">
+            Park Vehicle
+          </Button>
+        </Stack>
+      </form>
+
+      {openDialog && <DialogPark setOpen={setOpenDialog} data={dialogData} />}
+    </>
   );
 }
 
