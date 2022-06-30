4ea81ec2cae69c82f14a4a3d0dc36e9e7dc1800e
Jose Anthony Jovellanos <jove0610@gmail.com>
2022-07-01 05:14:20 +0800

    use cssbaseline

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import App from './App';
 import store from './redux/store';
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ReduxProvider store={store}>
+      <CssBaseline />
       <App />
     </ReduxProvider>
   </React.StrictMode>
