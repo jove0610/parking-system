ab04ccf03a378c98eb57b6b1790aff2d74a23575
Jose Anthony Jovellanos <jove0610@gmail.com>
2022-07-01 17:33:06 +0800

    update table UI to DataGrid

--- src/components/ParkingSpotsTable/index.jsx
+++ src/components/ParkingSpotsTable/index.jsx
@@ -1,43 +1,21 @@
 import React, { useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
 import { useSelector, useDispatch } from 'react-redux';
-import { nanoid } from '@reduxjs/toolkit';
 import dayjs from 'dayjs';
 
+import { IconButton, Tooltip } from '@mui/material';
 import { red } from '@mui/material/colors';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableFooter from '@mui/material/TableFooter';
-import TablePagination from '@mui/material/TablePagination';
-import Paper from '@mui/material/Paper';
-
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 
-import { getParkingSpots, unpark } from '../../redux/parkingSpotsSlice';
 import { getParkingEntrance } from '../../redux/parkingEntranceSlice';
+import { getParkingSpots, unpark } from '../../redux/parkingSpotsSlice';
 import { sizeOptions } from '../../constants';
 import prepareUnparkData from '../../helpers/prepareUnparkData';
-
-import StyledTableCell from '../StyledTableCell';
-import StyledTableRow from '../StyledTableRow';
 import DialogUnpark from './DialogUnpark';
-import TablePaginationActions from './TablePaginationActions';
 
-function ParkingSpotsTable() {
-  const dispatch = useDispatch();
+const useRows = () => {
   const parkingEntrance = useSelector(getParkingEntrance);
-  const [openDialog, setOpenDialog] = useState(false);
-  const [dialogData, setDialogData] = useState({});
   const parkingSpots = useSelector(getParkingSpots);
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - parkingSpots.length) : 0;
 
   // Used to display the size label given the size id
   const sizes = (() => {
@@ -48,14 +26,37 @@ function ParkingSpotsTable() {
     return output;
   })();
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+  const rows = parkingSpots.map((spot) => {
+    const row = {
+      action: spot,
+      id: spot.id,
+      parkSize: sizes[spot.sizeId],
+      plateNo: '',
+      startTime: '',
+    };
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
+    if (spot.isActive) {
+      row.plateNo = spot.plateNo;
+      row.startTime = dayjs(spot.startTime).format('h:mm A');
+    }
+
+    parkingEntrance.forEach((item, index) => {
+      row[String(item.id)] = spot.distances[index];
+    });
+
+    return row;
+  });
+
+  return rows;
+};
+
+function ParkingSpotsTable() {
+  const dispatch = useDispatch();
+  const rows = useRows();
+  const [pageSize, setPageSize] = useState(10);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [dialogData, setDialogData] = useState({});
+  const parkingEntrance = useSelector(getParkingEntrance);
 
   const onClickDel = (parkingSpot) => {
     const data = prepareUnparkData(parkingSpot);
@@ -64,100 +65,70 @@ function ParkingSpotsTable() {
     setOpenDialog(true);
   };
 
-  return (
-    <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-          <TableHead>
-            <StyledTableRow>
-              <StyledTableCell rowSpan={2} />
-              <StyledTableCell rowSpan={2}>Parking No.</StyledTableCell>
-              <StyledTableCell rowSpan={2}>Parking Size</StyledTableCell>
-              <StyledTableCell rowSpan={2}>Plate No.</StyledTableCell>
-              <StyledTableCell rowSpan={2}>Start Time</StyledTableCell>
-              <StyledTableCell colSpan={parkingEntrance.length} align="center">
-                Distance from
-              </StyledTableCell>
-            </StyledTableRow>
-
-            <StyledTableRow>
-              {parkingEntrance.map((option) => (
-                <StyledTableCell key={nanoid()} align="right">
-                  {option.label}
-                </StyledTableCell>
-              ))}
-            </StyledTableRow>
-          </TableHead>
-
-          <TableBody>
-            {(rowsPerPage > 0
-              ? parkingSpots.slice(
-                  page * rowsPerPage,
-                  page * rowsPerPage + rowsPerPage
-                )
-              : parkingSpots
-            ).map((row) => (
-              <StyledTableRow key={nanoid()}>
-                <StyledTableCell component="th" scope="row">
-                  {row.isActive && (
-                    <IconButton onClick={() => onClickDel(row)} sx={{ p: 0 }}>
-                      <RemoveCircleIcon sx={{ color: red[500] }} />
-                    </IconButton>
-                  )}
-                </StyledTableCell>
-                <StyledTableCell component="th" scope="row">
-                  {row.id}
-                </StyledTableCell>
-                <StyledTableCell style={{ width: 160 }}>
-                  {sizes[row.sizeId]}
-                </StyledTableCell>
-                <StyledTableCell style={{ width: 160 }}>
-                  {row.isActive && row.plateNo}
-                </StyledTableCell>
-                <StyledTableCell style={{ width: 160 }}>
-                  {row.isActive && row.startTime && (
-                    <>{dayjs(row.startTime).format('h:mm A')}</>
-                  )}
-                </StyledTableCell>
-                {row.distances.map((distance) => (
-                  <StyledTableCell key={nanoid()} align="right">
-                    {distance}
-                  </StyledTableCell>
-                ))}
-              </StyledTableRow>
-            ))}
-
-            {emptyRows > 0 && (
-              <StyledTableRow style={{ height: 53 * emptyRows }}>
-                <TableCell colSpan={6} />
-              </StyledTableRow>
+  const columns = (() => {
+    const output = [
+      {
+        field: 'action',
+        headerName: 'Action',
+        width: 70,
+        flex: 0.5,
+        sortable: false,
+        renderCell: (params) => (
+          <>
+            {params.value.isActive && (
+              <Tooltip title="Unpark vehicle" arrow>
+                <IconButton
+                  onClick={() => onClickDel(params.value)}
+                  sx={{ p: 0 }}
+                >
+                  <RemoveCircleIcon sx={{ color: red[500] }} />
+                </IconButton>
+              </Tooltip>
             )}
-          </TableBody>
+            {!params.isActive && <>&nbsp;</>}
+          </>
+        ),
+      },
+      { field: 'id', headerName: 'Parking No.', flex: 1 },
+      { field: 'parkSize', headerName: 'Parking Size', flex: 1 },
+      { field: 'plateNo', headerName: 'Plate No.', flex: 1 },
+      { field: 'startTime', headerName: 'Start Time', flex: 2 },
+    ];
 
-          <TableFooter>
-            <StyledTableRow>
-              <TablePagination
-                rowsPerPageOptions={[10, 15, 25, { label: 'All', value: -1 }]}
-                count={parkingSpots.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                  inputProps: {
-                    'aria-label': 'rows per page',
-                  },
-                  native: true,
-                }}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-              />
-            </StyledTableRow>
-          </TableFooter>
-        </Table>
-      </TableContainer>
+    parkingEntrance.forEach((item) => {
+      output.push({
+        field: String(item.id),
+        headerName: item.label,
+        flex: 1,
+      });
+    });
+
+    return output;
+  })();
+
+  return (
+    <div style={{ maxWidth: '90vw' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={pageSize}
+        autoHeight
+        disableColumnMenu
+        disableSelectionOnClick
+        onPageSizeChange={setPageSize}
+        rowsPerPageOptions={[10, 15, 30]}
+        sx={{
+          background: '#fff',
+          '$ .MuiDataGrid-sortIcon': {
+            display: 'block',
+            visibility: 'visible',
+            color: 'red',
+          },
+        }}
+      />
 
       {openDialog && <DialogUnpark setOpen={setOpenDialog} data={dialogData} />}
-    </>
+    </div>
   );
 }
 
