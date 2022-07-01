52ac9bfcd9096d9b87f63d931c2c21d548964dbe
Jose Anthony Jovellanos <jove0610@gmail.com>
2022-07-01 08:47:54 +0800

    improve styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,14 @@ import ParkingSpotsTable from './components/ParkingSpotsTable';
 
 function App() {
   return (
-    <Stack spacing="3em" px="3em" pb="5em">
+    <Stack
+      spacing="3em"
+      px="3em"
+      pb="5em"
+      sx={{ background: 'linear-gradient(to right, #00AAFF, #00FF6C)' }}
+    >
       <Stack
-        direction="row"
+        direction={{ xs: 'column', sm: 'row' }}
         spacing="2em"
         justifyContent="center"
         alignItems="center"
